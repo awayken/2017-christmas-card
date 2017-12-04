3e5effb25725d35d8ba005dfe67887d470a58d32
Miles Rausch <miles@milesrausch.com>
2017-12-03 20:51:49 -0600

    Simplify Navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,21 @@
 <template>
   <nav>
-    <a href="/">Home</a>
-    <a href="/page1">Page 1</a>
+    <a href="/" class="back">Back</a>
+    <a href="/">Start Over</a>
   </nav>
 </template>
+
+
+
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .back::before {
+    content: "< ";
+    display: inline;
+  }
+</style>
+
