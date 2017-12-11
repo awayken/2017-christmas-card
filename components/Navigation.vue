ee36c79af9b49370cb7b3965ff1ccad0715049d3
Miles Rausch <miles@milesrausch.com>
2017-12-11 07:10:22 -0600

    Add reading level toggle to navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,26 @@
 <template>
   <nav class="container">
     <nuxt-link to="/" class="back">Back</nuxt-link>
-    <a href="/">Start Over</a>
+    <ReadingLevelToggle :currentLevel="$store.state.readingLevel" :readingLevels="$store.state.readingLevels" />
   </nav>
 </template>
 
+<script>
+  import ReadingLevelToggle from '~/components/ReadingLevelToggle.vue'
+
+  export default {
+    components: {
+      ReadingLevelToggle
+    }
+  }
+</script>
+
 <style>
   nav {
+    align-items: center;
     display: flex;
     justify-content: space-between;
+    padding: 4px 0;
   }
 
   .back::before {
--- store/index.js
+++ store/index.js
@@ -3,7 +3,11 @@ import Vuex from 'vuex'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      readingLevel: 'young'
+      readingLevel: 'young',
+      readingLevels: {
+        young: 'Young Fruckles',
+        old: 'Old Fruckles'
+      }
     },
     mutations: {
       setReadingLevel (state, newReadingLevel) {
